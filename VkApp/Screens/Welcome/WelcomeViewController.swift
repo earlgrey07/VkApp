dedb01af71e3a408f4920385591aa4be0d1a0c31
earlgrey07 <earl_grey07@icloud.com>
2024-06-25 08:39:13 +0300

    Creating the Welcome Screen
    -Removed unnecessary Main Screen
    -Set Welcome screen as root controller for the app

---
+++ VkApp/Screens/Welcome/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.swift
+//  VkApp
+//
+//  Created by Данил on 25.06.2024.
+//  Copyright © 2024 earl-grey. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
