1c9b4b4eeeb6e78dff2e02f02d229f41b71c3439
earlgrey07 <earl_grey07@icloud.com>
2024-07-12 00:43:31 +0300

    Created GradientView
    -Added properties for gradient
    -gradientView added to WelcomViewController

---
+++ app/sources/Main/Components/GradientView.swift
@@ -0,0 +1,91 @@
+//
+//  GradientView.swift
+//  VkApp
+//
+//  Created by Данил on 11.07.2024.
+//  Copyright © 2024 earl-grey. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+    // MARK: - Interface
+    /// Gradient start color
+    var startColor: UIColor = .white {
+        didSet {
+            updateColors()
+        }
+    }
+    
+    /// Gradient end color
+    var endColor: UIColor = .blue {
+        didSet {
+            updateColors()
+        }
+    }
+    
+    /// Start of the gradient
+    var startLocation: CGFloat = 0 {
+        didSet {
+            updateLocations()
+        }
+    }
+    
+    /// End of the gradient
+    var endLocation: CGFloat = 1 {
+        didSet {
+            updateLocations()
+        }
+    }
+    
+    /// Gradient start point
+    var startPoint: CGPoint = .zero {
+        didSet {
+            updateStartPoint()
+        }
+    }
+    
+    /// Gradient end point
+    var endPoint: CGPoint = CGPoint(x: 0, y: 1) {
+        didSet {
+            updateEndPoint()
+        }
+    }
+    
+    /// Gradient type
+    var gradientType: CAGradientLayerType = .axial {
+        didSet {
+            updateType()
+        }
+    }
+    
+    // MARK: - Properties
+    private var gradientLayer: CAGradientLayer {
+        return layer as! CAGradientLayer
+    }
+    
+    override static var layerClass: AnyClass {
+        return CAGradientLayer.self
+    }
+    
+    // MARK: - Private
+    private func updateColors() {
+        gradientLayer.colors = [startColor.cgColor, endColor.cgColor]
+    }
+    
+    private func updateLocations() {
+        gradientLayer.locations = [startLocation as NSNumber, endLocation as NSNumber]
+    }
+    
+    private func updateStartPoint() {
+        gradientLayer.startPoint = startPoint
+    }
+    
+    private func updateEndPoint() {
+        gradientLayer.endPoint = endPoint
+    }
+    
+    private func updateType() {
+        gradientLayer.type = gradientType
+    }
+}
--- app/sources/Main/Screens/Welcome/WelcomeViewController.swift
+++ app/sources/Main/Screens/Welcome/WelcomeViewController.swift
@@ -9,9 +9,36 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    // MARK: - UI
+    private lazy var gradientView: GradientView = {
+        gradientViewSettings()
+    }()
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        setupGradientView()
+    }
+}
+
+// MARK: - UI
+extension WelcomeViewController {
+    private func gradientViewSettings() -> GradientView {
+        let view = GradientView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.startColor = .systemBlue
+        view.endColor = .black
+        return view
+    }
+    
+    private func setupGradientView() {
+        view.addSubview(gradientView)
+        
+        NSLayoutConstraint.activate([
+            gradientView.topAnchor.constraint(equalTo: view.topAnchor),
+            gradientView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            gradientView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            gradientView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        ])
     }
 }
